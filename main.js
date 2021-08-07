c99b2edd2caa7d581610c10ac0946479958904eb
John Doe <JohnDoe@gmail.com>
2021-08-07 15:31:29 +0200

    Finished

---
+++ main.js
@@ -0,0 +1,21 @@
+let addToDoButton =  document.getElementById('addToDo');
+let todocontainer =  document.getElementById('todoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click',function(){
+	
+	let paragraph = document.createElement('p');
+	paragraph.classList.add('paragraph-styling');
+	paragraph.innerText = inputField.value;
+	todocontainer.appendChild(paragraph);
+	inputField.value = "";
+	paragraph.addEventListener('click',function(){
+		paragraph.style.textDecoration = "line-through";
+
+	})
+	paragraph.addEventListener('dblclick',function(){
+		todoContainer.removeChild(paragraph);
+		
+		
+	})
+})
